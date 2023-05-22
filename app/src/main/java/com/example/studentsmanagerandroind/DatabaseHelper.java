593d178fe409f2b72c5d7676ef790591e9f7d489
enoquetembe <enoquetembetembe10@gmail.com>
2023-05-22 02:40:02 -0300

    feat: adding update student feature

--- app/src/main/java/com/example/studentsmanagerandroind/DatabaseHelper.java
+++ app/src/main/java/com/example/studentsmanagerandroind/DatabaseHelper.java
@@ -94,5 +94,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         String[] whereArgs = { name };
         db.delete(TABLE_ALUNOS, whereClause, whereArgs);
     }
+
+
+    public void update(String name, int age, String gender, String address, String telefone) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOME, name);
+        values.put(COLUMN_IDADE, age);
+        values.put(COLUMN_SEXO, gender);
+        values.put(COLUMN_MORADA, address);
+        values.put(COLUMN_TELEFONE, telefone);
+
+        db.update(TABLE_ALUNOS, values, COLUMN_NOME + " = ?", new String[]{name});
+        db.close();
+    }
 }
 
--- app/src/main/java/com/example/studentsmanagerandroind/StudentDatailsActivity.java
+++ app/src/main/java/com/example/studentsmanagerandroind/StudentDatailsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.studentsmanagerandroind.databinding.ActivityStudentDatailsBinding;
 
@@ -20,6 +21,12 @@ public class StudentDatailsActivity extends AppCompatActivity {
 
         databaseHelper = new DatabaseHelper(this);
 
+        binding.etDName.setEnabled(false);
+        binding.etDge.setEnabled(false);;
+        binding.etDSex.setEnabled(false);;
+        binding.etDAddress.setEnabled(false);
+        binding.etDTel.setEnabled(false);
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         studentName = name;
@@ -34,9 +41,26 @@ public class StudentDatailsActivity extends AppCompatActivity {
         binding.etDAddress.setText(address);
         binding.etDTel.setText(telefone);
 
+
+
          binding.btnDelete.setOnClickListener(v -> {
                 deleteStudent();
          });
+
+        binding.btnUpdate.setOnClickListener(v -> {
+            binding.etDName.setEnabled(true);
+            binding.etDge.setEnabled(true);;
+            binding.etDSex.setEnabled(true);;
+            binding.etDAddress.setEnabled(true);
+            binding.etDTel.setEnabled(true);
+
+            binding.btnUpdate.setClickable(false);
+        });
+
+        binding.btnDSave.setOnClickListener(v -> {
+            updateStudent();
+        });
+
     }
 
     private void deleteStudent() {
@@ -45,4 +69,17 @@ public class StudentDatailsActivity extends AppCompatActivity {
 
         this.finish();
     }
+
+    private void updateStudent() {
+        String name = binding.etDName.getText().toString();
+        int age = Integer.parseInt(binding.etDge.getText().toString());
+        String gender = binding.etDSex.getText().toString();
+        String address = binding.etDAddress.getText().toString();
+        String telefone = binding.etDTel.getText().toString();
+
+        databaseHelper.update(name, age, gender, address, telefone);
+
+        Toast.makeText(this, "Estudante atualizado com sucesso", Toast.LENGTH_SHORT).show();
+    }
+
 }
